bd39add1d2cec06aa02a8f0b5dbd68f20b4dc960
Lev Smirnov <levsmirnov2015@mail.ru>
2024-12-17 12:59:14 +0500

    create react app

---
+++ src/reportWebVitals.ts
@@ -0,0 +1,21 @@
+type ReportHandler = (metric: {
+  name: string;
+  value: number;
+  id: string;
+  delta: number;
+  entries: PerformanceEntry[];
+}) => void;
+
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+  if (onPerfEntry && typeof onPerfEntry === "function") {
+    import("web-vitals").then(({ onCLS, onFID, onFCP, onLCP, onTTFB }) => {
+      onCLS(onPerfEntry);
+      onFID(onPerfEntry);
+      onFCP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
