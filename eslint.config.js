11bd50b9d3708177ddd738e1afcb3b457c64be22
MaxRMNK <115977436+MaxRMNK@users.noreply.github.com>
2024-12-22 18:37:47 +0500

    feat: edit eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,13 @@
 import globals from 'globals';
 // import pluginJs from '@eslint/js';
-// import eslintConfigPrettier from 'eslint-config-prettier';
-// const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { languageOptions: { globals: globals.browser } },
   // pluginJs.configs.recommended,
-  // eslintConfigPrettier,
   eslintPluginPrettierRecommended,
+  eslintConfigPrettier,
   { ignores: ['/.git', '/.vscode', 'dist/*'] },
 ];
